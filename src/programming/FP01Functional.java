e5d642f45252186120772a678d0533bba8ee9b2e
mahaveerleo91 <mhvrmali@gmail.com>
2024-01-17 17:31:13 -0500

    refactoring

--- src/programming/FP01Functional.java
+++ src/programming/FP01Functional.java
@@ -8,15 +8,14 @@ import java.util.stream.IntStream;
 public class FP01Functional {
 
         public static void main(String[] args) {
-            List<Integer> numbers = List.of(12, 5, 76, 89, 35, 45, 6, 7, 78, 99, 67);
-            //printAllNumberInListFunctional(numbers);
-            //printEvenNumberInListFunctional(numbers);
-            //printonlyOddNum(numbers);
-            //printReverseNum(numbers);
+            List<Integer> numbers = new ArrayList(List.of(12, 5, 76, 89, 35, 45, 6, 7, 78, 99, 67));
+            printAllNumberInListFunctional(numbers);
+            printEvenNumberInListFunctional(numbers);
+            printOnlyOddNum(numbers);
+            printReverseNum(numbers);
 
-            List<Integer> mutableNumbers = new ArrayList<>(numbers);
-            Collections.sort(mutableNumbers, Collections.reverseOrder());
-            mutableNumbers.stream().forEach(System.out::println);
+            Collections.sort(numbers, Collections.reverseOrder());
+            numbers.forEach(System.out::println);
         }
 
         private static boolean isEven(int number){
@@ -33,17 +32,17 @@ public class FP01Functional {
                 // only allow even number
             .forEach(System.out::println);
         }
-        private static void printonlyOddNum(List<Integer> numbers){
-            numbers.stream().filter(number ->number%2==1).forEach(System.out::println);
+        private static void printOnlyOddNum(List<Integer> numbers){
+            numbers.stream()
+                    .filter(number ->number%2==1)
+                    .forEach(System.out::println);
         }
 
         private static void printReverseNum(List<Integer> numbers){
-            IntStream.range(0, numbers.size()).map(i ->numbers.size() - 1 - i).forEachOrdered(index ->
-            {int element = numbers.get(index); System.out.println(element);
-            });
+            IntStream.range(0, numbers.size())
+                    .map(i ->numbers.size() - 1 - i)
+                    .mapToObj(numbers::get)
+                    .forEach(System.out::println);
         }
-
-
-
 }
 
